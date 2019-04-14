0424c571caa8830e418781f83cd9fa387cdb67f5
Erick Nicolas <ericknicolas@gmail.com>
2019-04-14 08:49:01 +0200

    example of I/O files

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,14 @@
-name = "pepe"
-print(f"my name is {name}")
+myfile = open("helloworld.txt")
+print(myfile.read())
+myfile.seek(0)
+print(myfile.readlines())
+myfile.close()
+
+with open("helloworld.txt", mode="r") as myfile:
+    print(myfile.read())
+
+with open("helloworld.txt", mode="a") as myfile:
+    print(myfile.write("\ntercera linea"))
+
+with open("helloworld.txt", mode="r") as myfile:
+    print(myfile.read())
