266a0cf1d3f976f730e04bbba6453761b49b08fd
Erick Nicolas <ericknicolas@gmail.com>
2019-04-16 07:57:05 +0200

    for loop examples

--- helloworld.py
+++ helloworld.py
@@ -5,3 +5,14 @@ for num in list:
         print(f"Num {num} is even")
     else:
         print("Num {} is odd".format(num))
+
+for letter in "Hello World":
+    print(letter)
+
+my_list = [(0,1),(2,3),(4,5)]
+for a,b in my_list:
+    print(a)
+
+d = {"k1": 1, "k2": 2, "k3": 3}
+for key, value in d:
+    print(key)
