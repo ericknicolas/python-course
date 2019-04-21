fdb9c91a4f1c7dea297acdc37a2c5cd8bd0764e1
Erick Nicolas <ericknicolas@gmail.com>
2019-04-21 09:08:49 +0200

    summer of 69 problem

--- test/test.py
+++ test/test.py
@@ -1,25 +1,26 @@
-# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 
-# and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'¶
-#   blackjack(5,6,7) --> 18
-#   blackjack(9,9,9) --> 'BUST'
-#   blackjack(9,9,11) --> 19
+# SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 
+# (every 6 will be followed by at least one 9). Return 0 for no numbers.
+#   summer_69([1, 3, 5]) --> 9
+#   summer_69([4, 5, 6, 7, 8, 9]) --> 9
+#   summer_69([2, 1, 6, 9, 11]) --> 14
 
-def blackjack(a,b,c):
-
-    total = sum((a,b,c))
+def summer_69(arr):
+    flag = True
+    sum = 0
     
-    if 11 in (a,b,c):
-        total -= 10
+    for item in arr:
+        if item == 6:
+            flag = False
+        
+        if flag:
+            sum += item
+            print(sum)
+        
+        if item == 9:
+            flag = True
+    
+    return (sum)
 
-    if is_under21(total):
-        return total
-    else:
-        return "BUST"
-
-def is_under21(num):
-    return num <= 21
-
-# Check
-print(blackjack(5,6,7))
-print(blackjack(9,9,9))
-print(blackjack(9,9,11))
+print("Total {}".format(summer_69([1, 3, 5])))
+print("Total {}".format(summer_69([4, 5, 6, 7, 8, 9])))
+print("Total {}".format(summer_69([2, 1, 6, 9, 11])))
