01069692272e77bf48279f8b05825d00dcf2ae20
Erick Nicolas <ericknicolas@gmail.com>
2019-04-21 08:47:49 +0200

    blackjack problem

--- test/test.py
+++ test/test.py
@@ -1,15 +1,25 @@
-# PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
-#   paper_doll('Hello') --> 'HHHeeellllllooo'
-#   paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
+# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 
+# and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'¶
+#   blackjack(5,6,7) --> 18
+#   blackjack(9,9,9) --> 'BUST'
+#   blackjack(9,9,11) --> 19
 
-def paper_doll(text):
+def blackjack(a,b,c):
+
+    total = sum((a,b,c))
     
-    newText = ""
+    if 11 in (a,b,c):
+        total -= 10
 
-    for letter in text:
-        newText += letter * 3
-    
-    return newText
+    if is_under21(total):
+        return total
+    else:
+        return "BUST"
 
-print(paper_doll('Hello'))
-print(paper_doll('Mississippi'))
+def is_under21(num):
+    return num <= 21
+
+# Check
+print(blackjack(5,6,7))
+print(blackjack(9,9,9))
+print(blackjack(9,9,11))
