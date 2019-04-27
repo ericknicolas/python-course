210da11f9168d39f08635e4cab3534106a6b1966
Erick Nicolas <ericknicolas@gmail.com>
2019-04-27 18:09:57 +0200

    polymorphism advance example class

--- oop/oop.py
+++ oop/oop.py
@@ -1,16 +1,19 @@
-class Dog():
-
+class Animal():
+    
     def __init__(self, name):
         self.name = name
     
     def speak(self):
+        raise NotImplementedError("Subclass must implement this abstract method")
+
+
+class Dog(Animal):
+    
+    def speak(self):
         return self.name + " says woof!"
 
-class Cat():
+class Cat(Animal):
 
-    def __init__(self, name):
-        self.name = name
-    
     def speak(self):
         return self.name + " says meow!"
 
