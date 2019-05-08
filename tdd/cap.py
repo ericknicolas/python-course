88b5e8caf93f0a9b68805257d26229cd32fce2bb
Erick Nicolas <ericknicolas@gmail.com>
2019-05-08 08:03:44 +0200

    tdd example

---
+++ tdd/cap.py
@@ -0,0 +1,12 @@
+'''
+Capitalize first text letter
+'''
+
+def cap_text(text):
+    '''
+    Input string
+    Output first string letter capitalized
+    '''
+    return text.title()
+
+print(cap_text("hola"))
